853b4d44766587702b836cb276ca8a5e46ae111f
Moiz Haider <moizhaiderali74@gmail.com>
2021-10-15 18:23:13 +0500

    Product API Demo with Foreign Links

--- store/serializers.py
+++ store/serializers.py
@@ -1,9 +1,28 @@
 from abc import ABC
-
+from decimal import Decimal
+from .models import Product, Collection
 from rest_framework import serializers
+
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
 
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    collection = serializers.HyperlinkedRelatedField(
+        queryset=Collection.objects.all(),
+        view_name='collection-detail'
+    )
+    # collection = CollectionSerializer()
+    # collection = serializers.StringRelatedField()
+    # collection = serializers.PrimaryKeyRelatedField(
+    #     queryset=Collection.objects.all()
+    # )
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price + (product.unit_price * Decimal(0.17))
--- store/views.py
+++ store/views.py
@@ -3,13 +3,22 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Collection
+from .serializers import ProductSerializer, CollectionSerializer
 
 
 @api_view()
 def product_list(request):
-    return Response('ok')
+    queryset = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(queryset, many=True, context={'request': request})
+    return Response(serializer.data)\
+
+
+@api_view()
+def collection_detail(request, pk):
+    collection = get_object_or_404(Collection, pk=pk)
+    serializer = CollectionSerializer(collection)
+    return Response(serializer.data)
 
 
 @api_view()
@@ -19,7 +28,7 @@ def product_detail(request, id):
     #     serializer = ProductSerializer(product)
     #     return Response(serializer.data)
     # except Product.DoesNotExist:
-    #     return Response(status=status.HTTP_404_NOT_FOUND)
+    #     return Response(status=status.HTTP_404_NOT_FOUND)S
 
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
