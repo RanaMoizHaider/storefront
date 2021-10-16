e961f38419541407802c42782a3b91dc909cb094
Moiz Haider <moizhaiderali74@gmail.com>
2021-10-16 17:54:41 +0500

    Added DELETE

--- store/views.py
+++ store/views.py
@@ -27,7 +27,7 @@ def collection_detail(request, pk):
     return Response(serializer.data)
 
 
-@api_view(['GET', 'PUT', 'PATCH'])
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
 def product_detail(request, id):
     # try:
     #     product = Product.objects.get(pk=id)
@@ -40,9 +40,14 @@ def product_detail(request, id):
     if request.method == 'GET':
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-    elif request.method == 'PUT':
+    elif request.method == 'PUT' or request.method == 'PATCH':
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if product.orderitem_set.count() > 0:
+            return Response({'error': 'Product cannot be deleted as it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
